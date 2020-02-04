3b9ee28b992e0eb7bb36bda4651928c90825d170
Jean Harang <J.Harang@student.isartdigital.com>
2020-02-04 17:03:42 +0100

    Big Bang
    Beginning of universe

---
+++ Assets/Scripts/GizmoCreator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GizmoCreator : MonoBehaviour
+{
+    [Range(0.1f, 1)]
+    public float gizmoSize = 1;
+    public Color gizmoColor = Color.green;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = gizmoColor;
+        Gizmos.DrawWireSphere(transform.position, gizmoSize);
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public bool free;
+    public GameObject[] defenses;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (free)
+        {
+            if (Input.GetKeyDown("a"))
+            {
+
+            }
+        }
+    }
+}
