b3cc532c96a01eba4b9ddc090c2f78d74c8ec8f6
Aldaberto <a.cuello@student.isartdigital.com>
2020-03-14 20:07:39 +0100

    #Crash
    Crash crash crash

---
+++ Assets/Scripts/AI/Projo-Dot/P_Storm.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P_Storm : MonoBehaviour
+{
+    public GameObject storm;
+    void Update()
+    {
+        storm.transform.Rotate(0.0f, 0.0f, -12.0f);
+    }
+}
